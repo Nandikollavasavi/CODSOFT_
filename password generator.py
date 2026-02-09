783763c9ea400db6fb5fe85164fb2458322b58f6
Nandikollavasavi <nandikollavasavi67@gmail.com>
2026-02-09 22:58:40 +0530

    Update password generator.py

---
+++ password generator.py
@@ -0,0 +1,24 @@
+# Password Generator in Python
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+def main():
+    print("=== Password Generator ===")
+    try:
+        length = int(input("Enter desired password length: "))
+        
+        if length <= 0:
+            print("Password length must be greater than 0.")
+        else:
+            password = generate_password(length)
+            print(f"Generated Password: {password}")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+
+main()
